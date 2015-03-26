13ec9ddd34b08257b1b03c1ec3a42377f3655b1c
yanaor <yanaorlova17@gmail.com>
2015-03-26 23:43:08 +0200

    567

--- Test/searchproducts.py
+++ Test/searchproducts.py
@@ -1,33 +1,28 @@
 
 __author__ = 'yanaor'
 
-import os
+import unittest
 from selenium import webdriver
-# create a new Firefox session
-# get the path of IEDriverServer
-dir = os.path.dirname(__file__)
-chrome_driver_path = dir + "\Chromedriver.exe"
-# create a new Internet Explorer session
-driver = webdriver.Chrome(chrome_driver_path)
-driver.implicitly_wait(30)
-driver.maximize_window()
-# navigate to the application home page
-driver.get("http://demo.magentocommerce.com/")
-# get the search textbox
-search_field = driver.find_element_by_name("q")
-search_field.clear()
-# enter search keyword and submit
-search_field.send_keys("phones")
-search_field.submit()
-# get all the anchor elements which have product names displayed
-# currently on result page using find_elements_by_xpath method
-products = driver.find_elements_by_xpath("//h2[@class='product-name']/a")
-# get the number of anchor elements found
-print "Found " + str(len(products)) + " products:"
-# iterate through each anchor element and print the text that is
-# name of the product
-for product in products:
-    print product.text
-# close the browser window
-driver.quit()
-#jkashdka
+class SearchTests(unittest.TestCase):
+    def setUp(self):
+        # create a new Firefox session
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.driver.maximize_window()
+        # navigate to the application home page
+        self.driver.get("http://demo.magentocommerce.com/")
+    def test_search_by_category(self):
+        # get the search textbox
+        self.search_field = self.driver.find_element_by_name("q")
+        self.search_field.clear()
+        # enter search keyword and submit
+        self.search_field.send_keys("phones")
+        self.search_field.submit()
+        # get all the anchor elements which have product names
+        # displayed currently on result page using
+        # find_elements_by_xpath method
+        products = self.driver.find_elements_by_xpath("//h2[@class='product-name']/a")
+        self.assertEqual(2, len(products))
+    def tearDown(self):
+        # close the browser window
+        self.driver.quit()
