c806d05ea2d6d6a45d5868e5061f47dae57cb9c7
yanaor <yanaorlova17@gmail.com>
2015-03-27 12:31:47 +0200

    aksm

--- Test/searchtests.py
+++ Test/searchtests.py
@@ -4,13 +4,16 @@ __author__ = 'yanaor'
 import unittest
 from selenium import webdriver
 class SearchTests(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         # create a new Firefox session
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.driver.maximize_window()
+        cls.driver = webdriver.Firefox()
+        cls.driver.implicitly_wait(30)
+        cls.driver.maximize_window()
         # navigate to the application home page
-        self.driver.get("http://demo.magentocommerce.com/")
+        cls.driver.get("http://demo.magentocommerce.com/")
+        cls.driver.title
+
     def test_search_by_category(self):
         # get the search textbox
         self.search_field = self.driver.find_element_by_name("q")
@@ -22,10 +25,7 @@ class SearchTests(unittest.TestCase):
         # displayed currently on result page using
         # find_elements_by_xpath method
         products = self.driver.find_elements_by_xpath("//h2[@class='product-name']/a")
-        self.assertEqual(2, len(products))
-    def tearDown(self):
-        # close the browser window
-        self.driver.quit()
+        self.assertEqual(3, len(products))
     def test_search_by_name(self):
         # get the search textbox
         self.search_field = self.driver.find_element_by_name("q")
@@ -39,3 +39,9 @@ class SearchTests(unittest.TestCase):
         # find_elements_by_xpath method
         products = self.driver.find_elements_by_xpath("//h2[@class='product-name']/a")
         self.assertEqual(1, len(products))
+    @classmethod
+    def tearDownClass(cls):
+        # close the browser window
+        cls.driver.quit()
+  #  if __name__ == '__main__':
+    #    unittest.main()
