fee49503dc8bcb87d8a52853a7902598ed1b3e65
yanaor <yanaorlova17@gmail.com>
2015-03-30 18:09:38 +0300

    Alert

--- Test/homepagetest.py
+++ Test/homepagetest.py
@@ -1,6 +1,7 @@
 __author__ = 'yanaor'
 import unittest
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 
 
 class HomePageTest1(unittest.TestCase):
@@ -70,6 +71,32 @@ class HomePageTest1(unittest.TestCase):
         # close the shopping cart section
         close_button = self.driver.find_element_by_css_selector("div.minicart-wrapper a.close")
         close_button.click()
+
+    def test_language_options(self):
+        # list of expected values in Language dropdown
+        exp_options = ["ENGLISH", "FRENCH", "GERMAN"]
+        # empty list for capturing actual options displayed
+        # in the dropdown
+        act_options = []
+        # get the Your language dropdown as instance of Select class
+        select_language = Select(self.driver.find_element_by_id("select-language"))
+        # check number of options in dropdown
+        self.assertEqual(3, len(select_language.options))
+        # get options in a list
+        for option in select_language.options:
+            act_options.append(option.text)
+        # check expected options list with actual options list
+        self.assertListEqual(exp_options, act_options)
+        # check default selected option is English
+        self.assertEqual("ENGLISH", select_language.first_selected_option.text)
+        # select an option using select_by_visible text
+        select_language.select_by_visible_text("German")
+        # check store is now German
+        self.assertTrue("store=german" in self.driver.current_url)
+        # changing language will refresh the page,
+        # we need to get find language dropdown once again
+        select_language = Select(self.driver.find_element_by_id("select-language"))
+        select_language.select_by_index(0)
 
     @classmethod
     def tearDownClass(cls):
