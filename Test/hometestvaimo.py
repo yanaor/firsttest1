385630ddfad5c64e3be3b42cb02a71646108b60a
yanaor <yanaorlova17@gmail.com>
2015-05-21 18:23:40 +0300

    123

--- Test/hometestvaimo.py
+++ Test/hometestvaimo.py
@@ -10,9 +10,8 @@ def get_data(file_name):
     rows = []
     book = xlrd.open_workbook(file_name)
     sheet = book.sheet_by_index(0)
-    for row_idx in range(1, sheet.nrows):
-        rows.append(list(sheet.row_values(row_idx, 0, sheet.ncols)))
-        return rows
+    for row_idx in range(1, sheet.nrows):rows.append(list(sheet.row_values(row_idx, 0, sheet.ncols)))
+    return rows
 
 
 @ddt
@@ -28,16 +27,17 @@ class Authorization(unittest.TestCase):
     @unpack
     def test_valid_login(self, email, password, firstname, lastname):
         self.driver.find_element_by_link_text('LOG IN').click()
-        self.emailadressfield = self.driver.find_element_by_id("email")
-        self.emailadressfield.clear()
-        self.emailadressfield.send_keys(email)
-        self.passwordfield = self.driver.find_element_by_id("pass")
-        self.passwordfield.clear()
-        self.passwordfield.send_keys(password)
-        self.passwordfield.submit()
-        self.greeting = self.driver.find_element_by_class_name("hello").text
-        self.assertEqual(self.greeting, "Hello, %s %s!" % (firstname, lastname))
-        self.assertTrue(self.driver.find_element_by_link_text("LOG OUT").is_displayed())
+        emailadressfield = self.driver.find_element_by_id("email")
+        emailadressfield.clear()
+        emailadressfield.send_keys(email)
+        passwordfield = self.driver.find_element_by_id("pass")
+        passwordfield.clear()
+        passwordfield.send_keys(password)
+        passwordfield.submit()
+        greeting = self.driver.find_element_by_class_name("hello").text
+        self.assertEqual(greeting, "Hello, %s %s!" % (firstname, lastname))
+        logout = self.driver.find_element_by_link_text("LOG OUT")
+        self.assertTrue(logout.is_displayed())
 
     @classmethod
     def tearDownClass(self):
