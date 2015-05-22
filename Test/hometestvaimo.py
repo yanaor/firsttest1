a2d4bedf8d6a0545a91f0eb98d667c76f549e5fd
yanaor <yanaorlova17@gmail.com>
2015-05-22 12:35:26 +0300

    Test_vaimo_done

--- Test/hometestvaimo.py
+++ Test/hometestvaimo.py
@@ -23,7 +23,7 @@ class Authorization(unittest.TestCase):
         self.driver.maximize_window()
         self.driver.get("http://yanaorlova.vaimo.com/")
 
-    @data(*get_data('Data.xlsx'))
+    @data(*get_data('ValidData.xlsx'))
     @unpack
     def test_valid_login(self, email, password, firstname, lastname):
         self.driver.find_element_by_link_text('LOG IN').click()
@@ -39,6 +39,27 @@ class Authorization(unittest.TestCase):
         logout = self.driver.find_element_by_link_text("LOG OUT")
         self.assertTrue(logout.is_displayed())
 
+    @data(*get_data('InvalidData.xlsx'))
+    @unpack
+    def test_invalid_login(self, email, password):
+        self.driver.find_element_by_link_text('LOG IN').click()
+        emailadressfield = self.driver.find_element_by_id("email")
+        emailadressfield.clear()
+        emailadressfield.send_keys(email)
+        passwordfield = self.driver.find_element_by_id("pass")
+        passwordfield.clear()
+        if len(password)>=6:
+            passwordfield.send_keys(password)
+            passwordfield.submit()
+            errormessage = self.driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/ul/li/ul/li/span").text
+            self.assertEqual(errormessage, "Invalid login or password.")
+        else:
+            passwordfield.send_keys(password)
+            passwordfield.submit()
+            validation_advice = self.driver.find_element_by_id("advice-validate-password-pass").text
+            self.assertEqual(validation_advice, "Please enter 6 or more characters. Leading or trailing spaces will be ignored.")
+
+
     @classmethod
-    def tearDownClass(self):
+    def tearDown(self):
         self.driver.quit()
